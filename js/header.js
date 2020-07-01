0d972841a765c0cf3c0c15e78800b5fdf65844eb
sush096 <palsush096@gmail.com>
2020-07-01 17:17:29 +0530

    template in development

---
+++ js/header.js
@@ -0,0 +1,14 @@
+function toggle() {
+    var active = document.getElementById('side')
+    active.classList.toggle('sideactive')
+}
+
+function backdrop() {
+    var backdrop = document.getElementById('body')
+    backdrop.classList.toggle('backdrop')
+}
+
+function hamturn() {
+    var hamturn = document.getElementById('hamturn')
+    hamturn.classList.toggle('hamactive')
+}
